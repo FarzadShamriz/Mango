edf7621a39c562a4fb75bf5aea417bf643ae6790
Farzad Shamriz <farzadshamriz@outlook.com>
2025-12-15 17:16:17 -0500

    Configured dbcontext for AuthApi

---
+++ Mango.Services.AuthAPI/Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+namespace Mango.Services.AuthAPI.Data
+{
+    public class AppDbContext : IdentityDbContext<IdentityUser>
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
