89950b670d8057ecc4f931863fa2cff41cdcf85f
Farzad Shamriz <farzadshamriz@outlook.com>
2025-12-16 18:35:05 -0500

    Added login auth controller to web project

---
+++ Mongo.Web/Controllers/AuthController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mongo.Web.Models;
+using Mongo.Web.Services.IServices;
+
+namespace Mongo.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        private IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            LoginRequestDto loginRequestDto = new();
+            return View(loginRequestDto);
+        }
+
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult Logout()
+        {
+            LoginRequestDto loginRequestDto = new();
+            return View(loginRequestDto);
+        }
+    }
+}
