468cde97bcc20364baf1dea0e8fa7ac372943086
Farzad Shamriz <farzadshamriz@outlook.com>
2025-12-16 18:27:14 -0500

    Added auth service to the web project

---
+++ Mongo.Web/Services/AuthService.cs
@@ -0,0 +1,45 @@
+﻿using Mongo.Web.Models;
+using Mongo.Web.Services.IServices;
+using Mongo.Web.Utilities;
+
+namespace Mongo.Web.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly IBaseService _baseService;
+
+        public AuthService(IBaseService baseService)
+        {
+            _baseService = baseService;
+        }
+        public async Task<ResponseDto?> AssignRole(RegistrationRequestDto registrationRequestDto)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                Url = $"{SD.AuthAPIBase}/api/auth/AssignRole",
+                ApiType = SD.ApiType.POST,
+                Data = registrationRequestDto
+            });
+        }
+
+        public async Task<ResponseDto?> Login(LoginRequestDto loginRequestDto)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                Url = $"{SD.AuthAPIBase}/api/auth/Login",
+                ApiType = SD.ApiType.POST,
+                Data = loginRequestDto
+            });
+        }
+
+        public async Task<ResponseDto?> Register(RegistrationRequestDto registrationRequestDto)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                Url = $"{SD.AuthAPIBase}/api/auth/register",
+                ApiType = SD.ApiType.POST,
+                Data = registrationRequestDto
+            });
+        }
+    }
+}
---
+++ Mongo.Web/Services/IServices/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using Mongo.Web.Models;
+
+namespace Mongo.Web.Services.IServices
+{
+    public interface IAuthService
+    {
+        Task<ResponseDto?> Login(LoginRequestDto loginRequestDto);
+        Task<ResponseDto?> Register(RegistrationRequestDto registrationRequestDto);
+        Task<ResponseDto?> AssignRole(RegistrationRequestDto registrationRequestDto);
+    }
+}
