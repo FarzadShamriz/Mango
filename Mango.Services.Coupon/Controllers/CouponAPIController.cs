8a094f80025d43e26c1124f94e74e80fedf08f4a
farzad <farza@DESKTOP-UDVL642>
2025-12-21 18:08:24 -0500

    Added annotation to authorize the user based on the role (Coupont Add, Delete, Update)

--- Mango.Services.Coupon/Controllers/CouponAPIController.cs
+++ Mango.Services.Coupon/Controllers/CouponAPIController.cs
@@ -79,6 +79,7 @@ namespace Mango.Services.Coupon.Controllers
 
 
         [HttpPost]
+        [Authorize(Roles = "ADMIN")]
         public ResponseDto Post([FromBody] CouponDto couponDto)
         {
             try
@@ -98,6 +99,7 @@ namespace Mango.Services.Coupon.Controllers
 
 
         [HttpPut]
+        [Authorize(Roles = "ADMIN")]
         public ResponseDto Put([FromBody] CouponDto couponDto)
         {
             try
@@ -118,6 +120,7 @@ namespace Mango.Services.Coupon.Controllers
 
         [HttpDelete]
         [Route("{id:int}")]
+        [Authorize(Roles = "ADMIN")]
         public ResponseDto Delete(int id)
         {
             try
