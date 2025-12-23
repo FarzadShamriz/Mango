51fca6edbb049fab3916c0fd8819854da7f97140
FShamriz <fshamriz@pmialliancegroup.com>
2025-12-23 10:21:41 -0500

    Implemented product API and related config files

---
+++ Mango.Services.ProductAPI/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Mango.Services.ProductAPI.Models;
+using Mango.Services.ProductAPI.Models.DTOs;
+
+namespace Mango.Services.ProductAPI
+{
+    public class MappingProfile
+    {
+
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config => {
+                config.CreateMap<ProductDto, Product>().ReverseMap();
+            });
+
+            return mappingConfig;
+        }
+
+    }
+}
