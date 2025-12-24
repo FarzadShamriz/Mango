3f7de0f79fc705f8ef798d24feab37bdc8c4af2a
FShamriz <fshamriz@pmialliancegroup.com>
2025-12-24 14:27:46 -0500

    ShoppingCart project init

---
+++ Mango.Services.ShoppingCartAPI/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Mango.Services.ShoppingCartAPI.Models;
+using Mango.Services.ShoppingCartAPI.Models.DTOs;
+
+namespace Mango.Services.ProductAPI
+{
+    public class MappingProfile
+    {
+
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config => {
+                config.CreateMap<CartDetailsDto, CartDetails>().ReverseMap();
+            });
+
+            return mappingConfig;
+        }
+
+    }
+}
---
+++ Mango.Services.ShoppingCartAPI/Models/DTOs/CartHeaderDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mango.Services.ShoppingCartAPI.Models.DTOs
+{
+    public class CartHeaderDto
+    {
+        public int CartHeaderId { get; set; }
+        public string? UserId { get; set; }
+        public string? CouponCode { get; set; }
+        public double? Dicsount { get; set; }
+        public double? CartTotal { get; set; }
+    }
+}
