ad7ca5ee56e7fbe05d4fb5c6b0f598665de37b04
FShamriz <fshamriz@pmialliancegroup.com>
2025-12-24 13:58:46 -0500

    Home page modification and product details page implementation in web project

--- Mongo.Web/Controllers/HomeController.cs
+++ Mongo.Web/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Mongo.Web.Models;
+using Mongo.Web.Services.IServices;
+using Newtonsoft.Json;
 using System.Diagnostics;
 
 namespace Mongo.Web.Controllers
@@ -7,15 +10,45 @@ namespace Mongo.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IProductService _productService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IProductService productService)
         {
             _logger = logger;
+            _productService = productService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            List<ProductDto> products = new();
+            ResponseDto response = await _productService.GetAllProductsAsync();
+
+            if (response != null && response.IsSuccess)
+            {
+                products = JsonConvert.DeserializeObject<List<ProductDto>>(response.Result.ToString());
+                TempData["success"] = response?.Message;
+            }
+            else
+            {
+                TempData["error"] = response?.Message;
+            }
+
+            return View(products);
+        }
+
+        [Authorize]
+        public async Task<IActionResult> ProductDetails(int productId)
+        {
+            var responseModel = await _productService.GetProductByIdAsync(productId);
+            if(responseModel != null && responseModel.IsSuccess)
+            {
+                var model = JsonConvert.DeserializeObject<ProductDto>(responseModel.Result.ToString());
+                return View(model);
+            }
+
+            TempData["error"] = "Product Not Found!!!";
+            return RedirectToAction(nameof(Index));
+
         }
 
         public IActionResult Privacy()
