51fb2f90f0303bb39b240cd86ced3569aa09e139
FShamriz <fshamriz@pmialliancegroup.com>
2025-12-31 12:25:59 -0500

    Added required DBContext, Dtos and Models for Order API

---
+++ Mango.Services.OrderAPI/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+namespace Mango.Services.OrderAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
