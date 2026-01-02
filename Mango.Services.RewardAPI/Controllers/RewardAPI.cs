1e576edeb2a855a1d175af42aea04d590f545d67
Farzad Shamriz <farzadshamriz@outlook.com>
2026-01-02 16:18:56 -0500

    Auto migration in project execution implemented in program.cs. API controller added to the reward project

---
+++ Mango.Services.RewardAPI/Controllers/RewardAPI.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.RewardAPI.Controllers
+{
+    [Route("api/reward")]
+    [ApiController]
+    public class RewardAPI : ControllerBase
+    {
+    }
+}
